a1066e976d7aa2ab5674a6240826c680c5d0914d
Mohamad Aboda <41721894+Mohamad-Aboda@users.noreply.github.com>
2020-09-15 07:11:09 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,8 @@
+import mnist_loader()
+train, val, test = mnist_loader.load_data_wrapper()
+
+import network
+net = network.Network([784, 30, 10])
+# this will take some time to train depends on your machine 
+net.SGD(train, 30, 10, 3, test_data = test)
+
